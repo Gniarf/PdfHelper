c0e016dc4b90476ecf087a7c001b3b1ce51b268d
Gniarf <nicolas.bellino@gmail.com>
2023-04-14 11:28:11 +0200

    project initialisation

---
+++ PdfHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PdfHelper;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        if (args == null || args.Length == 0)
+        {
+            Console.WriteLine("Must receive path of target pdf file to extract its content.");
+        }
+    }
+}
